fc038ae99bead12053ecbb562cfa8854984100ed
nicholasbrean91 <31775539+nicholasbrean91@users.noreply.github.com>
2017-09-12 16:38:51 -0400

    Update and rename hello_world_GUI.py to Adress.py

--- Adress.py
+++ Adress.py
@@ -2,7 +2,7 @@
 # Created on: 12-Sep-2017
 # Created for: ICS3U
 # Daily Assignment - Unit0-02
-# This peogram displays Hello World but in GUI
+# This program dislplays a GUI Adress code line
 
 
 import ui
