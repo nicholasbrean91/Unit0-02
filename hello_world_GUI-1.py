4ebc0edf88ca6bb9d93d797b2f481bd423cb3dcf
Nicholas <Nicholas.brean@ocsbstudent.ca>
2017-09-12 16:20:24 -0400

    Init

---
+++ hello_world_GUI-1.py
@@ -0,0 +1,11 @@
+# Created by: Nicholas Brean
+# Created on: 12-Sep-2017
+# Created for: ICS3U
+# Daily Assignment - Unit0-02
+# This peogram displays Hello World but in GUI
+
+
+import ui
+
+v = ui.load_view()
+v.present('sheet')
